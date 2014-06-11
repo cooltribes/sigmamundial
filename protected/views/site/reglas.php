c7d1bbe317afeb89e570800b7e0c16623e76e225
dduque <dduque@upsidecorp.ch>
2014-06-11 19:01:19 -0430

    Reglas de juego

--- protected/views/site/reglas.php
+++ protected/views/site/reglas.php
@@ -19,7 +19,146 @@ $this->pageTitle=Yii::app()->name . ' - Términos y Condiciones.';
                 <!--TEXTO-->
                 <div class="row">
                     <div class="col-sm-10 col-sm-offset-1">
-                        TEXTO PARA LAS REGLAS
+                         <div class="row text-center"> Normas que regulan el concurso de quiniela Sigma Es Mundial </div>
+							<div>
+								<br/><br/>
+								Definición: La quiniela consiste en pronosticar resultados para los encuentros de la Copa Mundial de Fútbol Brasil 2014 
+<br/><br/>
+								<strong>REGLAS GENERALES</strong> 
+<br/><br/>
+La inscripción al juego de la quiniela Sigma es Mundial es completamente gratuita y requiere un registro en la aplicación con datos reales del usuario. Este es un requisito indispensable para activar la cuenta con su usuario de Twitter y validarla desde su correo personal.
+<br/><br/>
+Cada día se activarán los encuentros a disputarse según el cronograma correspondiente. existiendo la posibilidad de ser un (1) partido por día hasta un máximo de cuatro (4). Los encuentros se activarán a las 12 am del día en cuestión. Para aquellos días que no tengan encuentros fijados según calendario no existirá la opción de predicción para el usuario.
+<br/><br/>
+Para cada encuentro existe un tiempo máximo para realizar la predicción del resultado. Cada encuentro está condicionado a cerrar el proceso de predicción diez (10) minutos antes del pitazo inicial del encuentro (según hora oficial del organizador mundial). 
+<br/><br/>
+Cada usuario inscrito puede hacer solo un pronóstico de resultado por encuentro, por tanto, el usuario podrá realizar un máximo de 64 predicciones.
+<br/><br/>
+Los resultados de la predicción del usuario serán publicados en la red social Twitter (previa aprobación del usuario al momento de la inscripción) automáticamente. La predicción será almacenada en los datos del usuario. La publicación en Twitter representa el soporte del usuario.
+<br/><br/>
+Para que un usuario gane con la predicción de un encuentro se tomará en cuenta el resultado del encuentro solo en el tiempo reglamentario (90 minutos) por lo que la predicción se comparará contra dicho resultado sin importar el caso en el que un encuentro continúe en tiempo extra y lanzamientos desde el punto penal. El resultado del partido será publicado en la cuenta oficial de Twitter @SigmaOficial 
+<br/><br/>
+Cada acierto de predicción del usuario generará un premio, el cual se hará llegar al correo electrónico ingresado en la inscripción. Además sumará puntos acumulables por todo el torneo para la entrega de premios por fase y un gran premio final.
+<br/><br/>
+Los usuarios ganadores serán publicados vía Twitter desde la cuenta oficial. 
+<br/><br/>
+
+<strong>DATOS SOBRE LA INSCRIPCIÓN Y ACCESO DEL USUARIO</strong>
+<br/><br/>
+No se cobrará bajo ninguna razón por la inscripción para poder participar en el juego. Es totalmente gratuito.
+<br/><br/>
+Es indispensable el uso de una cuenta personal de Twitter para realizar el proceso de inscripción.
+<br/><br/>
+Los datos ingresados a través del formulario de registro deben ser datos reales ya que no se permite la modificación de los mismos. 
+<br/><br/>
+Los datos del usuario menor de edad serán enviados al correo del padre o la madre, haciendo de esta una notificación como control parental.
+<br/><br/>
+El usuario recibirá un correo con un link de activación de su cuenta, dicho enlace permitirá la validación de la cuenta desde su correo electrónico. 
+<br/><br/>
+La empresa tiene la potestad de eliminar usuarios menores de edad que sean solicitados por sus padres. Así como también eliminar a los usuarios con múltiples cuentas comprobadas.
+<br/><br/>
+Las cuentas que no sean válidadas desde el correo serán eliminadas automáticamente.
+<br/><br/>
+El usuario podrá acceder a su cuenta desde cualquier dispositivo con acceso a internet
+<br/><br/>
+En caso de olvidar la contraseña, el usuario podrá acceder a una opción de recuperación de password con un link a su correo.
+<br/><br/>
+Si un usuario desea eliminar su cuenta deberá enviar un correo electrónico con su solicitud a: erasedata@sigmatiendas.com
+<br/><br/>
+
+<strong>SOBRE LA ACTIVACIÓN DEL BOTON DE PREDICCIONES</strong>
+<br/><br/>
+A partir del día 12 de Junio de 2014 y hasta el 13 de Julio de 2014 se activarán el (los) boton(es) para acceder a las predicciones por encuentro regidas por el calendario oficial.
+<br/><br/>
+En total se activarán 25 días de predicciones para completar los 64 partidos pautados en la contienda mundial de fútbol. Se activarán al comienzo del día pautado por la organización del mundial. Y estará activo hasta diez (10) minutos previos al inicio de la contienda. 
+<br/><br/>
+
+<strong>SOBRE LA DESACTIVACIÓN DEL BOTON DE PREDICCIÓN </strong>
+<br/><br/>
+El o los botones de predicción están configurados para desactivarse de dos (2) maneras:
+<br/><br/>
+1.- Al cumplirse el tiempo estipulado para el proceso de predicción: Diez (10) minutos antes del pitazo inicial del encuentro (según hora oficial del organizador mundial). 
+<br/><br/>
+2.- Al realizar el usuario su predicción del encuentro..
+<br/><br/>
+
+<strong>SOBRE LAS PREDICCIONES</strong>
+<br/><br/>
+El usuario podrá realizar la predicción que desee proporcionando los goles del encuentro resultando en ganador local, empate o ganador visitante. 
+<br/><br/>
+Los resultados deben escribirse en números positivos, sin fracciones o decimales.
+<br/><br/>
+El usuario puede ingresar su predicción de forma manual o usando los botones de las cajas de predicción
+<br/><br/>
+Cada usuario podrá realizar una predicción por encuentro para un máximo de 64 predicciones en total. No existe una opción para modificar. Al aceptar y publicar la apuesta queda fijada en el sistema.
+<br/><br/>
+Cada predicción estará identificada con la bandera y nombre del país acompañado de la fecha y hora programada por el organizador del mundial, para así evitar confusiones del usuario.
+<br/><br/>
+Cada una de las predicciones serán publicadas en la cuenta de Twitter del usuario automáticamente, haciendo el uso del Hashtag y menciones a @sigmaoficial. Esta será el comprobante de predicción del usuario. 
+<br/><br/>
+
+<strong>SOBRE GANAR UNA PREDICCIÓN</strong>
+<br/><br/>
+Un usuario gana al pronosticar el resultado de un encuentro de la Copa Mundial de Fútbol. El resultado proporcionado por el usuario debe ser el mismo del encuentro en cantidad de goles a favor y en contra, durante el tiempo reglamentario del juego (90 min)
+<br/><br/>
+<em>Ejemplo1:    Usuario1:    BRA 2 vs CRO 1	Resultado real:	BRA      03 vs CRO 01</em>
+<br/><br/>
+Esta predicción no es ganadora al no acertar el resultado exacto del encuentro. Aún así el Usuario 1 acumulará un (1) punto al haber acertado el ganador del encuentro.
+<br/><br/>
+<em>Ejemplo2:   Usuario1:    BRA 1 vs CRO 1	Resultado real:	BRA    01 vs CRO 01</em>
+<br/><br/>
+Esta predicción es <strong>ganadora</strong>. En este caso el usuario acertó el resultado exacto del encuentro por lo que es ganador y acumulará tres (3) puntos.
+<br/><br/>
+
+<strong>SOBRE LOS PUNTOS DE LA QUINIELA</strong>
+<br/><br/>
+Los puntos que se otorgarán y acumularán sobre los resultados de las predicciones se colocarán de la siguiente forma:
+<br/><br/>
+- <strong>3 PUNTOS</strong>: por acertar el resultado exacto del encuentro en el tiempo reglamentario (90 minutos)
+<br/><br/>
+- <strong>1 PUNTO</strong>: por acertar el equipo ganador (o empate) aunque los goles de la predicción del usuario no sean los mismos que los del resultado final en el tiempo reglamentario (90 minutos)
+<br/><br/>
+- <strong>SIN PUNTOS</strong>: Aquellas predicciones donde el resultado difiera al resultado final en el tiempo reglamentario (90 minutos)
+<br/><br/>
+
+<strong>SOBRE LOS PREMIOS</strong>
+<br/><br/>
+Existen tres categorías de premiación para este juego
+<br/><br/>
+
+<strong>1. Premiación por acierto del encuentro</strong>
+<br/><br/>
+Estos premios están estipulados para aquellos usuarios que hayan acertado en su predicción el resultado exacto del encuentro en el tiempo reglamentario. No son válidos resultados de tiempo extra o penales..
+<br/><br/>
+Los premios serán enviados automáticamente al correo electrónico del usuario ganador en un lapso de 5 a 10 minutos después de terminado el tiempo reglamentario del encuentro. Explicando las normas de uso y reclamo del premio.
+<br/><br/>
+
+<strong>2. Premiación por acumulación de puntos por fase</strong>
+<br/><br/>
+Se activarán premios para los usuarios por fases de juegos del mundial. Por fase solo existirá un usuario ganador y ese será aquel que tenga más puntos acumulados de la quiniela en dicha etapa. Estan dividas de la siguiente manera:
+<br/><br/>
+<strong>Fase 1: Fase de grupos.</strong> El ganador con mayor acumulado de puntos será informado por correo de su premio y como disfrutarlo en un lapso no mayor a un (1) día de haber terminado dicha etapa.
+<br/><br/>
+<strong>Fase 2: Fase de Octavos de final.</strong> El ganador con mayor acumulado de puntos será informado por correo de su premio y como disfrutarlo. En un lapso no mayor a un (1) dia de haber terminado dicha etapa.
+<br/><br/>
+<strong>Fase 3: Fase de Cuartos de final.</strong> El ganador con mayor acumulado de puntos será informado por correo de su premio y como disfrutarlo. En un lapso no mayor a un (1)  dia de haber terminado dicha etapa.
+<br/><br/>
+<strong>Fase 4: Etapa de Juegos Final.</strong> (Semifinales y Tercer Lugar) El ganador con mayor acumulado de puntos será informado por correo de su premio y como disfrutarlo. En un lapso no mayor a un (1)  dia de haber terminado el partido en el que se disputa el tercer lugar del mundial.
+<br/><br/>
+
+<strong>3. Premiación por acumulación de puntos en toda la quiniela</strong>
+<br/><br/>
+El (los) usuario(s) con mayor número de puntos acumulados durante todo el juego serán invitados por correo electrónico a un evento final del mundial en la ciudad de San Cristóbal, Estado Táchira, para hacer entrega del gran premio final.
+<br/><br/>
+El usuario ganador debe estar presente en dicho evento para poder reclamar su premio
+<br/><br/>
+En caso de empate de puntos al final de la quiniela, el usuario que esté presente en el evento gana el premio final.
+<br/><br/>
+En caso de empate de puntos y presencia de varios usuarios en el evento, se realizará una trivia frente al público del evento donde deberán responder una ronda de preguntas hasta que finalice un solo usuario como ganador.
+<br/><br/>
+
+</div>
+
                     </div>
                 </div>
                 <!--BOTON-->
