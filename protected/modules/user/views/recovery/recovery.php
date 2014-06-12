a53db7e366d2eb51cecafb439c4a9c4b7b3695f3
nramirez <nramirez@upsidecorp.ch>
2014-06-12 18:17:09 -0430

    hadga

--- protected/modules/user/views/recovery/recovery.php
+++ protected/modules/user/views/recovery/recovery.php
@@ -1,62 +1,61 @@
-<?php $this->pageTitle=Yii::app()->name . ' - Recuperar contraseña';
+<?php $this->pageTitle = Yii::app()->name . ' - Recuperar contraseña';
 ?>
 
 <div class="row">
-	<?php 
-	if (Yii::app()->user->hasFlash('recoveryMessage')) { 
-	 	?>
+    <?php
+    if (Yii::app()->user->hasFlash('recoveryMessage')) {
+        ?>
         <div class="alert in alert-block fade alert-success text_align_center">
             <?php echo Yii::app()->user->getFlash('recoveryMessage'); ?>
         </div>
-    	<?php
-    }else{
+        <?php
+    } else {
         ?>
-        <div class="col-md-6 col-md-offset-3 panel-gris register-panel">
-			<div class="row">
-	            <div class="col-md-12">
-	                <div class="col-md-12 panel-header">
-	                    <h3>Recuperar contraseña</h3>                      
-	                </div>
-	            </div>
-	        </div>
-	        <div class="row panel-content">
-				<div class="col-md-12">
-	                <div class="row">
-	                	<div class="col-md-12">
-	                		<div class="form">
-								<?php $form2=$this->beginWidget('bootstrap.widgets.BsActiveForm', array(
-								    //'action'=>'user/login',
-								    'layout' => BsHtml::FORM_LAYOUT_HORIZONTAL,
-								    'id'=>'recovery-form',
-								    'enableAjaxValidation'=>false,
-								    'clientOptions'=>array(
-									    'validateOnSubmit'=>true,
-									),
-								  'htmlOptions' => array('enctype'=>'multipart/form-data'),
-								)); ?>
+        <div class="col-md-6 col-md-offset-3 panel-azul">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-md-12 panel-header">
+                        <h3>Recuperar contraseña</h3>                      
+                    </div>
+                </div>
+            </div>
+            <div class="row panel-content">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-sm-10 col-sm-offset-1">
+                            <?php $form2=$this->beginWidget('bootstrap.widgets.BsActiveForm', array(
+                                //'action'=>'user/login',
+                                //'layout' => BsHtml::FORM_LAYOUT_HORIZONTAL,
+                                'id'=>'recovery-form',
+                                'enableAjaxValidation'=>false,
+                                'clientOptions'=>array(
+                                        'validateOnSubmit'=>true,
+                                    ),
+                              'htmlOptions' => array('enctype'=>'multipart/form-data',
+                                  'class' => 'form'),
+                            )); ?>
 
-								<?php echo $form2->errorSummary(array($form)); ?>
+                            <?php echo $form2->errorSummary(array($form)); ?>
 
-								<div class="form-group">
-								  <?php 
-								  echo $form2->textField($form, "login_or_email", array());
-								  ?>
-								</div>       
+                            <div class="form-group">
+                              <?php 
+                              echo $form2->textField($form, "login_or_email", array());
+                              ?>
+                            </div>       
 
-								<div class="form-group text-center">
-								  <?php echo BsHtml::submitButton("Recuperar", array(
-								    "color" => BsHtml::BUTTON_COLOR_DANGER,
-								    'size' => BsHtml::BUTTON_SIZE_LARGE,
-								  )); ?>
-								</div>
-								<?php $this->endWidget(); ?>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php
-	}
-	?>
+                            <div class="form-group text-center">
+                              <?php echo BsHtml::submitButton("Recuperar", array(
+                                "color" => BsHtml::BUTTON_COLOR_DANGER,
+                                'size' => BsHtml::BUTTON_SIZE_LARGE,
+                              )); ?>
+                            </div>
+                            <?php $this->endWidget(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
 </div>
--- themes/bootstrap/views/site/error.php
+++ themes/bootstrap/views/site/error.php
@@ -2,14 +2,33 @@
 /* @var $this SiteController */
 /* @var $error array */
 
-$this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
+$this->pageTitle = Yii::app()->name . ' - Error';
+$this->breadcrumbs = array(
+    'Error',
 );
 ?>
+<div class="col-md-6 col-md-offset-3 panel-azul panel-error">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-12 panel-header">
+                <h3>Error</h3>                      
+            </div>
+        </div>
+    </div>
+    <div class="row panel-content">
+        <div class="col-md-12">
+            <div class="row codigo">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <?php echo $code; ?>
 
-<h2>Error <?php echo $code; ?></h2>
+                </div>
+            </div>
+            <div class="row mensaje">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <?php echo CHtml::encode($message); ?>
 
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
